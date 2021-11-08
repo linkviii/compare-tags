983385a8ed1a83d5a4988337b931e62af0006ce2
linkviii <LinkEighth@gmail.com>
2021-11-08 17:07:59 -0500

    needs more jquerey

--- js/main.js
+++ js/main.js
@@ -1,7 +1,14 @@
 /*  */
+//import jquery
+import "./jquery.js";
+/* GLOBAL */
+export const usingTestData = false;
+// export const usingTestData = true;
+/* INIT */
 document.addEventListener("DOMContentLoaded", function (event) {
     //do work
     console.log("init page");
+    giveFeedback("Page loaded");
     const table = document.getElementById("tag-table");
     const tableBody = document.getElementById("table-body");
     const headerRow = document.getElementById("header-row");
@@ -74,4 +81,79 @@ document.addEventListener("DOMContentLoaded", function (event) {
         newRow(numRow);
     });
 });
+/* END Init */
+export async function getAniList(userName) {
+    if (usingTestData) {
+        console.warn("Using test data.");
+        giveFeedback("Using test data");
+        const url = "res/anilist_example.json";
+        let job = await fetch(url).then(response => response.json());
+        return job;
+    }
+    const query = `
+  query ($userName: String) { 
+      MediaListCollection(userName: $userName, type: ANIME) {
+          hasNextChunk
+          user {
+              id
+          }
+          lists {
+              name
+              status
+              entries {
+                  mediaId
+                  score
+                  progress
+                  startedAt { year month day } 
+                  completedAt { year month day }
+                  media {
+                      duration
+                      episodes
+                      format
+                      title {
+                          romaji english native userPreferred
+                      }
+                  }
+              }
+          }
+      }
+  }
+  `; // Could probably munch the whitespace with a regex but no real need to
+    const variables = {
+        userName: userName
+    };
+    // Define the config we'll need for our Api request
+    const url = 'https://graphql.anilist.co', options = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
+        },
+        body: JSON.stringify({
+            query: query,
+            variables: variables
+        })
+    };
+    const response = await fetch(url, options);
+    const foo = await response.json();
+    if (foo.errors) {
+        console.error(foo.errors);
+        return new Error("Error fetching list");
+    }
+    const data = foo.data.MediaListCollection;
+    if (data.hasNextChunk) {
+        console.warn("TODO: next chunk not implemented yet.");
+    }
+    return data;
+}
+// 
+function giveFeedback(str, sec = 5) {
+    const time = sec * 1000;
+    const feedback = $("#feedback");
+    feedback.text(str);
+    // feedback[0].textContent = str;
+    setTimeout(function () {
+        feedback.text("");
+    }, time);
+}
 //# sourceMappingURL=main.js.map
