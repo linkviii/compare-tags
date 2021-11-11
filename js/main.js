3b4f18937e29d9b8115beff88e19aafe9a486e58
linkviii <LinkEighth@gmail.com>
2021-11-11 13:22:36 -0500

    remove times table. Add season info

--- js/main.js
+++ js/main.js
@@ -11,77 +11,6 @@ document.addEventListener("DOMContentLoaded", function (event) {
     //do work
     console.log("init page");
     giveFeedback("Page loaded");
-    const table = document.getElementById("tag-table");
-    const tableBody = document.getElementById("table-body");
-    const headerRow = document.getElementById("header-row");
-    const button = document.getElementById("button");
-    const buttonRow = document.getElementById("row-button");
-    const buttonCol = document.getElementById("col-button");
-    let numRow = 0;
-    let numCol = 0;
-    // map header to column number
-    const colMap = new Map();
-    function newCol(numCol) {
-        /* Make header */
-        {
-            const head = document.createElement("th");
-            head.textContent = `${numCol}`;
-            headerRow.append(head);
-        }
-        /* Fix old rows */
-        for (let rowVal = 1; rowVal <= tableBody.children.length; rowVal++) {
-            // for (let row of tableBody.children){
-            const row = tableBody.children[rowVal - 1];
-            let rowColCount = row.children.length;
-            for (; rowColCount <= numCol; rowColCount++) {
-                const data = document.createElement("td");
-                // data.textContent = `${rowVal}, ${rowColCount}`;
-                data.textContent = `${rowVal * rowColCount}`;
-                row.append(data);
-            }
-        }
-    }
-    ;
-    function newRow(numRow) {
-        /* new row */
-        const row = document.createElement("tr");
-        {
-            // const data = document.createElement("td");
-            const data = document.createElement("th");
-            data.textContent = `${numRow}`;
-            row.append(data);
-        }
-        for (let i = 1; i <= numCol; i++) {
-            const data = document.createElement("td");
-            data.textContent = `${i * numRow}`;
-            row.append(data);
-        }
-        tableBody.append(row);
-    }
-    ;
-    button.addEventListener("click", function () {
-        console.log("ay, got clicked");
-        numRow++;
-        numCol++;
-        console.log(`row ${numRow}, col ${numCol}`);
-        newCol(numCol);
-        newRow(numRow);
-    });
-    // ------------------
-    buttonCol.addEventListener("click", function () {
-        console.log("ay, got clicked");
-        numCol++;
-        console.log(`row ${numRow}, col ${numCol}`);
-        newCol(numCol);
-    });
-    // ------------------
-    // ------------------
-    buttonRow.addEventListener("click", function () {
-        console.log("ay, got clicked");
-        numRow++;
-        console.log(`row ${numRow}, col ${numCol}`);
-        newRow(numRow);
-    });
 });
 /* END Init */
 export async function getAniList(userName) {
@@ -195,6 +124,13 @@ export async function getAniAnime(input) {
           rank
         }
         genres 
+        season
+        seasonYear
+        startDate {
+          year
+          month
+          day
+        }
       }
     }
   `; // Could probably munch the whitespace with a regex but no real need to
@@ -333,6 +269,7 @@ export function test() {
             }
         }
     }
+    //
     for (let row of genreTable.rows.values()) {
         let anime = testData[row.id];
         const tr = genreTable.body.children[row.index];
--- js/main.ts
+++ js/main.ts
@@ -20,102 +20,6 @@ document.addEventListener("DOMContentLoaded", function (event) {
   giveFeedback("Page loaded");
 
 
-  const table = document.getElementById("tag-table");
-  const tableBody = document.getElementById("table-body");
-  const headerRow = document.getElementById("header-row");
-
-  const button = document.getElementById("button");
-  const buttonRow = document.getElementById("row-button");
-  const buttonCol = document.getElementById("col-button");
-
-  let numRow = 0;
-  let numCol = 0;
-
-  // map header to column number
-  const colMap = new Map<string, number>();
-
-  function newCol(numCol: number) {
-
-    /* Make header */
-    {
-      const head = document.createElement("th");
-      head.textContent = `${numCol}`;
-      headerRow.append(head);
-    }
-
-    /* Fix old rows */
-    for (let rowVal = 1; rowVal <= tableBody.children.length; rowVal++) {
-      // for (let row of tableBody.children){
-      const row = tableBody.children[rowVal - 1];
-      let rowColCount = row.children.length;
-      for (; rowColCount <= numCol; rowColCount++) {
-        const data = document.createElement("td");
-        // data.textContent = `${rowVal}, ${rowColCount}`;
-        data.textContent = `${rowVal * rowColCount}`;
-        row.append(data);
-      }
-
-    }
-
-  };
-
-  function newRow(numRow: number) {
-    /* new row */
-    const row = document.createElement("tr");
-    {
-      // const data = document.createElement("td");
-      const data = document.createElement("th");
-      data.textContent = `${numRow}`;
-      row.append(data);
-    }
-    for (let i = 1; i <= numCol; i++) {
-      const data = document.createElement("td");
-      data.textContent = `${i * numRow}`;
-      row.append(data);
-
-    }
-    tableBody.append(row);
-  };
-
-  button.addEventListener("click", function () {
-    console.log("ay, got clicked");
-    numRow++;
-    numCol++;
-    console.log(`row ${numRow}, col ${numCol}`);
-
-
-    newCol(numCol);
-
-
-    newRow(numRow);
-
-
-  });
-
-
-  // ------------------
-  buttonCol.addEventListener("click", function () {
-    console.log("ay, got clicked");
-
-    numCol++;
-    console.log(`row ${numRow}, col ${numCol}`);
-
-    newCol(numCol);
-
-  });
-
-  // ------------------
-  // ------------------
-
-  buttonRow.addEventListener("click", function () {
-    console.log("ay, got clicked");
-    numRow++;
-    console.log(`row ${numRow}, col ${numCol}`);
-
-    newRow(numRow);
-
-
-  });
 
 
 });
@@ -264,6 +168,13 @@ export async function getAniAnime(input: string): Promise<any | Error> {
           rank
         }
         genres 
+        season
+        seasonYear
+        startDate {
+          year
+          month
+          day
+        }
       }
     }
   `; // Could probably munch the whitespace with a regex but no real need to
@@ -423,6 +334,12 @@ class HTable {
 
 }
 
+interface ADate {
+  year:number;
+  month:number;
+  day:number;
+}
+
 interface Tag {
   id: number;
   name: string;
@@ -438,6 +355,9 @@ interface Anime {
   title: Title;
   tags: Tag[];
   genres: string[];
+  season: "WINTER" | "SPRING" | "SUMMER" | "FALL";
+  seasonYear: number;
+  startDate: ADate;
 }
 
 const tagThreshold = 50;
@@ -478,6 +398,7 @@ export function test() {
     }
 
   }
+  //
   for (let row of genreTable.rows.values()) {
     let anime = testData[row.id] as Anime;
     const tr = genreTable.body.children[row.index];
